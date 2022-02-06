f877ab988a6394c119e72ec2170751465ff43dc1
Chuck Cottrill <chuck.cottrill@telo.com>
2022-02-05 23:58:43 -0500

    add database, internationalization settings

--- messages/settings/database.py
+++ messages/settings/database.py
@@ -1,4 +1,5 @@
-
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 from settings import env
 
@@ -18,6 +19,14 @@ DATABASES = {
         "NAME": env(PGNAME, defaults="postgres"),
         "USER": env(PGUSER, defaults="user"),
         "PASSWORD": env(PGPASSWORD, defaults="password"),
-    }
+    },
+    "sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": env(SQLITE, default=f"{BASE_DIR}/db.sqlite3"),
+    },
+    "cache": {
+        "ENGINE": "django.db.backends.redis???",
+        "NAME": env(CACHEDB, default="redis"),
+    },
 }
 
---
+++ messages/settings/internationalization.py
@@ -0,0 +1,11 @@
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+from settings import env
+
+LANGUAGE_CODE = env(LANGUAGE_CODE, default="en-us")
+TIME_ZONE = env(TIME_ZONE, default="UTC")
+USE_TZ = env.bool(USE_TZ, default=True)
+USE_I18N = True
+USE_L10N = True
+
